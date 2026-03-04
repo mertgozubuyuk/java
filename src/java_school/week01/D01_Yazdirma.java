82be767f897590623340073444c73c80485c5b26
Mert Gozubuyuk <mertgozubuyuk26@gmail.com>
2026-03-04 23:43:35 +0300

    Initial commit

---
+++ src/java_school/week01/D01_Yazdirma.java
@@ -0,0 +1,9 @@
+package java_school.week01;
+
+public class D01_Yazdirma {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Mert Gözübüyük");
+        System.out.println("Java Öğreniyorum");
+    }
+}
